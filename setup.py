18ff2651a38f35a88bef71f63f2b6d6222bed71c
Prods <pedro.rodriguez.email@gmail.com>
2018-04-27 23:19:48 -0400

    Updated setup description.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='An Extended JSON Parser Library',  # Required
+    description='Extended JSON Parser Library for Python',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
