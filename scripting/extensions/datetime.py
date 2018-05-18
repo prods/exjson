935309d062e7a628748c02246bdbb067d1d83018
Prods <pedro.rodriguez.email@gmail.com>
2018-05-18 17:40:36 -0400

    Refactored #2: date formatting
    This will allow for formatting to be reused on other date functions.

--- scripting/extensions/datetime.py
+++ scripting/extensions/datetime.py
@@ -14,17 +14,19 @@ def _get_now_utc():
 def now(*args):
     """Gets current date and time. Default format is ISO8601+TZ."""
     now = _get_now()
+    frmt = None
     if len(args) > 0:
-        return now.strftime(args[0])
-    return now.isoformat()
+        frmt = args[0]
+    return _format(now, frmt)
 
 
 def now_utc(*args):
     """Gets current UTC date and time. Default format is ISO8601+TZ."""
     now = _get_now_utc()
+    frmt = None
     if len(args) > 0:
-        return now.strftime(args[0])
-    return now.isoformat() + "-00:00"
+        frmt = args[0]
+    return _format(now, frmt)
 
 
 def now_add(*args):
@@ -33,3 +35,12 @@ def now_add(*args):
 
 def now_utc_add(*args):
     return "PENDING"
+
+
+def _format(dt:datetime, format):
+    if format is not None:
+        return dt.strftime(format)
+    if dt.utcoffset() is None:
+        return dt.isoformat() + "-00:00"
+    else:
+        return dt.isoformat()
