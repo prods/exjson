170b557278f1031d79643c5a9bfa8213a0a42679
Prods <pedro.rodriguez.email@gmail.com>
2018-05-28 20:27:33 -0400

    Added base for custom date format calculation

--- scripting/extensions/datetime.py
+++ scripting/extensions/datetime.py
@@ -2,40 +2,45 @@ from datetime import datetime, timedelta
 
 from dateutil.tz import tzlocal
 
+def get_week(date: datetime.date):
+    return date.strftime('%w').zfill('0', 2)
+
+
 _DATETIME_FORMAT_MAP = [
-    (0, "dddd", "%A"),  # Weekday as locale’s full name. Ex. "Monday"
-    (1, "ddd", "%a"),  # Weekday as locale’s abbreviated name. Ex. "Mon"
-    # "": "%w",  # Weekday as a decimal number, where 0 is Sunday and 6 is Saturday. Ex. "1"
-    (3, "dd", "%d"),  # Day of the month as a zero-padded decimal number. Ex. "30"
-    (4, "d", "%-d"),  # Day of the month as a decimal number. (Platform specific) Ex. "30"
-    (5, "MMMM", "%B"),  # Month as locale’s full name. Ex. "September"
-    (6, "MMM", "%b"),  # Month as locale’s abbreviated name. Ex. "Sep"
-    (7, "MM", "%m"),  # Month as a zero-padded decimal number. Ex. "09"
-    (8, "M", "%-m"),  # Month as a decimal number. (Platform specific) Ex. "9"
-    (9,"yyyy", "%Y"),  # Year with century as a decimal number. Ex. "2013"
-    (10, "y", "%y"),  # Year without century as a zero-padded decimal number. Ex. "13"
-    (11, "HH", "%H"),  # Hour (24-hour clock) as a zero-padded decimal number. Ex. "07"
-    (12, "H", "%-H"),  # Hour (24-hour clock) as a decimal number. (Platform specific) Ex. "7"
-    (13, "hh", "%I"),  # Hour (12-hour clock) as a zero-padded decimal number. Ex. "07"
-    (14, "h", "%-I"),  # Hour (12-hour clock) as a decimal number. (Platform specific) Ex. "7"
-    (15, "tt", "%p"),  # Locale’s equivalent of either AM or PM. Ex. "AM"
-    (16, "mm", "%M"),  # Minute as a zero-padded decimal number. Ex. "06"
-    (17, "m", "%-M"),  # Minute as a decimal number. (Platform specific) Ex. "6"
-    (18, "ss", "%S"),  # Second as a zero-padded decimal number. Ex. "05"
-    (19, "s", "%-S"),  # Second as a decimal number. (Platform specific) Ex. "5"
-    (20, "f", "%f"),  # Microsecond as a decimal number, zero-padded on the left. Ex. "000000"
-    (21, "zzz", "%Z"),  # Time zone name (empty string if the object is naive). Ex. ""
-    (22, "z", "%z"),  # UTC offset in the form +HHMM or -HHMM (empty string if the the object is naive). Ex. ""
-    # "": "%j",  # Day of the year as a zero-padded decimal number. Ex. "273"
+    (0, "dddd", "%A", None),  # Weekday as locale’s full name. Ex. "Monday"
+    (1, "ddd", "%a", None),  # Weekday as locale’s abbreviated name. Ex. "Mon"
+    (2, "w", "%w", None), # Weekday as a decimal number, where 0 is Sunday and 6 is Saturday. Ex. "1"
+    (3, "ww", "", get_week), # Weekday as zero padded number
+    (3, "dd", "%d", None),  # Day of the month as a zero-padded decimal number. Ex. "30"
+    (4, "d", "%-d", None),  # Day of the month as a decimal number. (Platform specific) Ex. "30"
+    (5, "MMMM", "%B", None),  # Month as locale’s full name. Ex. "September"
+    (6, "MMM", "%b", None),  # Month as locale’s abbreviated name. Ex. "Sep"
+    (7, "MM", "%m", None),  # Month as a zero-padded decimal number. Ex. "09"
+    (8, "M", "%-m", None),  # Month as a decimal number. (Platform specific) Ex. "9"
+    (9,"yyyy", "%Y", None),  # Year with century as a decimal number. Ex. "2013"
+    (10, "y", "%y", None),  # Year without century as a zero-padded decimal number. Ex. "13"
+    (11, "HH", "%H", None),  # Hour (24-hour clock) as a zero-padded decimal number. Ex. "07"
+    (12, "H", "%-H", None),  # Hour (24-hour clock) as a decimal number. (Platform specific) Ex. "7"
+    (13, "hh", "%I", None),  # Hour (12-hour clock) as a zero-padded decimal number. Ex. "07"
+    (14, "h", "%-I", None),  # Hour (12-hour clock) as a decimal number. (Platform specific) Ex. "7"
+    (15, "tt", "%p", None),  # Locale’s equivalent of either AM or PM. Ex. "AM"
+    (16, "mm", "%M", None),  # Minute as a zero-padded decimal number. Ex. "06"
+    (17, "m", "%-M", None),  # Minute as a decimal number. (Platform specific) Ex. "6"
+    (18, "ss", "%S", None),  # Second as a zero-padded decimal number. Ex. "05"
+    (19, "s", "%-S", None),  # Second as a decimal number. (Platform specific) Ex. "5"
+    (20, "f", "%f", None),  # Microsecond as a decimal number, zero-padded on the left. Ex. "000000"
+    (21, "zzz", "%Z", None),  # Time zone name (empty string if the object is naive). Ex. ""
+    (22, "z", "%z", None),  # UTC offset in the form +HHMM or -HHMM (empty string if the the object is naive). Ex. ""
+    #(23, "y": "%j",  # Day of the year as a zero-padded decimal number. Ex. "273"
     # "": "%-j",  # Day of the year as a decimal number. (Platform specific) Ex. "273"
     # "": "%U",
     # Week number of the year (Sunday as the first day of the week) as a zero padded decimal number. All days in a new year preceding the first Sunday are considered to be in week 0. Ex. "39"
     # "": "%W",
     # Week number of the year (Monday as the first day of the week) as a decimal number. All days in a new year preceding the first Monday are considered to be in week 0. Ex. "39"
-    (27, "F", "%c"),  # Locale’s appropriate date and time representation. Ex. "Mon Sep 30 07:06:05 2013"
-    (28, "D", "%x"),  # Locale’s appropriate date representation. Ex. "09/30/13"
-    (29, "T", "%X"),  # Locale’s appropriate time representation. Ex. "07:06:05"
-    (30, "Z", "%Y-%m-%dT%H:%m:%s.%f%z")  # Full ISO-8601
+    (27, "F", "%c", None),  # Locale’s appropriate date and time representation. Ex. "Mon Sep 30 07:06:05 2013"
+    (28, "D", "%x", None),  # Locale’s appropriate date representation. Ex. "09/30/13"
+    (29, "T", "%X", None),  # Locale’s appropriate time representation. Ex. "07:06:05"
+    (30, "Z", "%Y-%m-%dT%H:%m:%s.%f%z", None)  # Full ISO-8601
 ]
 
 _formats_cache = {}
